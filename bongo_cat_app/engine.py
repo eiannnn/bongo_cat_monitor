6e575e81e7d258e86a6288a7dbe56255a9f54571
Eian <eian@Erics-Mac-mini.local>
2026-01-11 19:38:16 -0800

    Fix WPM double counting

--- bongo_cat_app/engine.py
+++ bongo_cat_app/engine.py
@@ -71,7 +71,7 @@ class BongoCatEngine:
         self.last_time_sent = 0
         
         # Industry-standard WPM
-        self.chars_per_word = 5.0
+        self.chars_per_word = 10
         self.min_animation_speed = 500
         self.max_animation_speed = 40
         
